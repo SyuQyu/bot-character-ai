52fab99ee623ed6aa6c7489752f390c4e57545d7
Pandu Utomo <panxzzlyn@gmail.com>
2024-05-05 12:27:48 +0700

    feat

---
+++ constants/listCharacters.js
@@ -0,0 +1,48 @@
+const characterId = [
+    {
+        name: "**Hosino AI**",
+        id: "GVvUaE4lE3TYhGP6vr_WLzexdysOOK5LdaIjdSV8jgY",
+    },
+    {
+        name: "**Yae Miko**",
+        id: "Dvm_7p9ea1uPKtENq18-tjJqHpFkdmP-nFreeGx00s0",
+    },
+    {
+        name: "**Makima**",
+        id: "wqikzRvE_AM0V2ZQ5ujq3rignjHm91x7ieYbX0ExpMk",
+    },
+    {
+        name: "**Frieren**",
+        id: "jeC02ArWFsboN8OyW16Q734QTnnWza_wwQl7gu_Wy0o",
+    },
+    {
+        name: "**Kafka**",
+        id: "RYdl1drUQGDft0dj7TMOgYVCZIz96gPFSGb8NT3dhCY",
+    },
+    {
+        name: "**Napoleon Bonaparte**",
+        id: "7wS3gFoudRBK-YpX3Etq8ag4V2c9_a1RCwPbfLlVQtE",
+    },
+    {
+        name: "**Albedo**",
+        id: "ry8gF_zBtaSQgVTQ-LQrS98yxtCNZ86SRH7R1L0g6iU",
+    },
+    {
+        name: "**Sukuna**",
+        id: "e4YGobLn_1SNmMxoDU0Pt25tYvGjV4Rm-LaoWQdkZts",
+    },
+    {
+        name: "**Yor Forger**",
+        id: "WsqG34NBsbCr3hxN7gJA_y5khYtVQzTD71IqdtfO57Y",
+    },
+    {
+        name: "**Raiden Shogun**",
+        id: "RQrrOj-UNdEV2_PC5D03US-27MZ7EUtaRH_husjbRQA",
+    },
+    {
+        name: "**RPG Roleplay**",
+        id: "FKArATeiEf0I9fxOIAwUOW3KTlm8yjW-eflbHMCqyBE",
+    },
+];
+
+module.exports = characterId;
